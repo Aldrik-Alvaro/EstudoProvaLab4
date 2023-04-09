46f05258cf30e1dc81cf20a6d23b686bb933d101
Aldrik Alvaro <aldrikfariaalvaro@gmail.com>
2023-04-09 20:32:14 -0300

    sql

---
+++ notacao.sql
@@ -0,0 +1,47 @@
+Pessoal, conforme conversado em aula, nossa primeira avaliação ocorrerá dia 11/04/2023. 
+
+O conteúdo será Mapeamento de Tabela, Repository, Serviço e Controller. Para ser avaliada a aplicação deve rodar.
+
+Dependencies
+    Spring Boot DevTools
+    Spring Data JPA 
+
+    (MySQL Driver) or (Oracle Driver)
+
+    (opcional) - Lombok
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
